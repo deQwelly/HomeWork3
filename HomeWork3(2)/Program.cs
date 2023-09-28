d5e618d1772bd8af2770bfc9a391a5cfe1764b10
anton <anton@DESKTOP-0F6GIFB>
2023-09-28 15:53:30 +0300

    Добавьте файлы проекта.

---
+++ HomeWork3(2)/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace hw3_2_
+{
+    internal class Program
+    {
+        enum DayOfWeek
+        {
+            Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+        }
+        static void Main(string[] args)
+        {
+            // Упражнение 1
+            Console.WriteLine("Упражнение 1: Дана последовательность из 10 чисел. Определить, является" +
+                " ли эта последовательность\r\nупорядоченной по возрастанию. В случае отрицательного ответа" +
+                " определить\r\nпорядковый номер первого числа, которое нарушает данную последовательность.\r\nИспользовать break.");
+            int[] sequence = new int[10];
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write($"Введите {i + 1}-ое число последовательности: ");
+                    sequence[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                /// for (int i = 0; i < sequence.Length; i ++) { Console.WriteLine(sequence[i]); }
+                bool flag = true;
+                for (int i = 0; i < sequence.Length - 1; i++)
+                {
+                    if (sequence[i] >= sequence[i + 1])
+                    {
+                        Console.WriteLine($"Возрастающая последовательность прерывается после {i + 1}-го элемента");
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    Console.WriteLine("Последовательность является возрастающей");
+                }
+            }
+            catch (System.OverflowException)
+            {
+                Console.WriteLine("Вы ввели слишком большое или, наоборот, маленькое число");
+            }
+            catch
+            {
+                Console.WriteLine("Недопустимый формат ввода данных. Возможно, вы ввели дробное число, нечисловые символы или не ввели ничего вовсе");
+            }
+
+            // Упражнение 2
+            Console.WriteLine("\nУпражнение 2: Игральным картам условно присвоены следующие порядковые номера в зависимости от\r\n" +
+                "их достоинства: «валету» — 11, «даме» — 12, «королю» — 13, «тузу» — 14.\r\nПорядковые номера остальных" +
+                " карт соответствуют их названиям («шестерка»,\r\n«девятка» и т. п.). По заданному номеру карты k (6 <=k <= 14)" +
+                " определить достоинство\r\nсоответствующей карты.");
+            Console.Write("Введите число от 6 до 14: ");
+            try
+            {
+                switch (Convert.ToByte(Console.ReadLine()))
+                {
+                    case 6: Console.WriteLine("Шесть"); break;
+                    case 7: Console.WriteLine("Семь"); break;
+                    case 8: Console.WriteLine("Восемь"); break;
+                    case 9: Console.WriteLine("Девять"); break;
+                    case 10: Console.WriteLine("Десять"); break;
+                    case 11: Console.WriteLine("Валет"); break;
+                    case 12: Console.WriteLine("Дама"); break;
+                    case 13: Console.WriteLine("Король"); break;
+                    case 14: Console.WriteLine("Туз"); break;
+                    default: Console.WriteLine("Вы ввели символы, не входящие в диапазон от 6 до 14"); break;
+                }
+            }
+            catch (System.OverflowException)
+            {
+                Console.WriteLine("Вы ввели слишком большое или, наоборот, маленькое число");
+            }
+            catch
+            {
+                Console.WriteLine("Недопустимый формат ввода данных. Возможно, вы ввели дробное число, нечисловые символы или не ввели ничего вовсе");
+            }
+
+            // Упражнение 3
+            Console.WriteLine("\nУпражнение 3: Напишите программу, которая принимает на входе строку и производит выходные\r\nданные в соответствии со указанной таблицей");
+            Console.Write("Введите данные: ");
+            switch (Console.ReadLine().ToLower())
+            {
+                case "jabroni": Console.WriteLine("Patron Tequila"); break;
+                case "school counselor": Console.WriteLine("Anything whith Alcohol"); break;
+                case "programmer": Console.WriteLine("Hipster Craft Beer"); break;
+                case "bike gang member": Console.WriteLine("Moonshine"); break;
+                case "politician": Console.WriteLine("Your tax dollars"); break;
+                case "rapper": Console.WriteLine("Chistal"); break;
+                default: Console.WriteLine("Beer"); break;
+            }
+
+            // Упражнение 4
+            Console.WriteLine("\nУпражнение 4: Составить программу, которая в зависимости от порядкового номера дня недели (1, 2,\r\n...,7)" +
+                " выводит на экран его название (понедельник, вторник, ..., воскресенье).");
+            Console.Write("Введите номер дня недели: ");
+            try
+            {
+                if (byte.TryParse(Console.ReadLine(), out byte day))
+                {
+                    Console.WriteLine(Enum.GetName(typeof(DayOfWeek), day - 1));
+                }
+                else
+                {
+                    Console.WriteLine("Указанное число может не соответствовать диапазону. Так же, возможно, вы ввели дробное число, нечисловые символы или не ввели ничего вовсе");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка");
+            }
+
+            // Упражнение 5
+            Console.WriteLine("\nУпражнение 5: Создать массив строк. При помощи foreach обойти весь массив. При встрече элемента" +
+                "\r\n\"Hello Kitty\" или \"Barbie doll\" необходимо положить их в “сумку”, т.е. прибавить к\r\nрезультату. Вывести на экран сколько кукол в “сумке”.");
+            byte count = 0;
+            string[] market = new string[10];
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"Введите {i + 1}-ый элемент: ");
+                    market[i] = Console.ReadLine();
+                }
+                foreach (string i in market)
+                {
+                    if ((i.ToLower().Equals("hello kitty")) | (i.ToLower().Equals("barbie doll")))
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine($"Кукол в сумке - {count}");
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork3/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace hw3
+{
+    internal class Program
+    {
+        enum Months
+        {
+            January = 31,
+            February = 59,
+            March = 90,
+            April = 120,
+            May = 151,
+            June = 181,
+            July = 212,
+            August = 243,
+            September = 273,
+            October = 304,
+            November = 334,
+            December = 365
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 4.1: Написать программу, которая читает с экрана число от 1 до 365 (номер дня\r\n" +
+                "в году), переводит этот число в месяц и день месяца. (високосный год не учитывать).\nУпражнение 4.2: Добавить к " +
+                "задаче из предыдущего упражнения проверку числа введенного\r\nпользователем. Если число меньше 1 или больше 365, " +
+                "программа должна вырабатывать\r\nисключение, и выдавать на экран сообщение.\nДомашнее задание 4.1: Изменить программу" +
+                " из упражнений 4.1 и 4.2 так, чтобы она\r\nучитывала год (високосный или нет). Год вводится с экрана. (Год високосный," +
+                " если он\r\nделится на четыре без остатка, но если он делится на 100 без остатка, это не високосный\r\nгод. Однако, если" +
+                " он делится без остатка на 400, это високосный год.)");
+            Console.Write("\nВведите номер года: ");
+            if (int.TryParse(Console.ReadLine(), out int year))
+            {
+                if (year > 0)
+                {
+                    byte leap = 0;
+                    if (((year % 4 == 0) & (year % 100 != 0)) | (year % 400 == 0))
+                    {
+                        Console.Write("Вы выбрали високосный год\n" +
+                            "Введите порядковый номер дня в этом году (от 1 до 366): ");
+                        leap++;
+                    }
+                    else
+                    {
+                        Console.Write("Вы выбрали не високосный год\n" +
+                            "Введите порядковый номер дня в этом году (от 1 до 365): ");
+                    }
+                    if (int.TryParse(Console.ReadLine(), out int day))
+                    {
+                        if ((day >= 1) & (day <= 31))
+                        {
+                            Console.WriteLine($"Январь {day}");
+                        }
+                        else if ((day >= 32) & (day <= (59 + leap)))
+                        {
+                            Console.WriteLine($"Февраль {day - 31}");
+                        }
+                        else if ((day >= (60 + leap)) & (day <= (90 + leap)))
+                        {
+                            Console.WriteLine($"Март {day - (59 + leap)}");
+                        }
+                        else if ((day >= (91 + leap)) & (day <= (120 + leap)))
+                        {
+                            Console.WriteLine($"Апрель {day - (90 + leap)}");
+                        }
+                        else if ((day >= (121 + leap)) & (day <= (151 + leap)))
+                        {
+                            Console.WriteLine($"Май {day - (120 + leap)}");
+                        }
+                        else if ((day >= (152 + leap)) & (day <= (181 + leap)))
+                        {
+                            Console.WriteLine($"Июнь {day - (151 + leap)}");
+                        }
+                        else if ((day >= (182 + leap)) & (day <= (212 + leap)))
+                        {
+                            Console.WriteLine($"Июль {day - (181 + leap)}");
+                        }
+                        else if ((day >= (213 + leap)) & (day <= (243 + leap)))
+                        {
+                            Console.WriteLine($"Август {day - (212 + leap)}");
+                        }
+                        else if ((day >= (244 + leap)) & (day <= (273 + leap)))
+                        {
+                            Console.WriteLine($"Сентябрь {day - (243 + leap)}");
+                        }
+                        else if ((day >= (274 + leap)) & (day <= (304 + leap)))
+                        {
+                            Console.WriteLine($"Октябрь {day - (273 + leap)}");
+                        }
+                        else if ((day >= (305 + leap)) & (day <= (334 + leap)))
+                        {
+                            Console.WriteLine($"Ноябрь {day - (304 + leap)}");
+                        }
+                        else if ((day >= (335 + leap)) & (day <= (365 + leap)))
+                        {
+                            Console.WriteLine($"Декабрь {day - (334 + leap)}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Вы ввели значение, не входящее в указанный диапазон");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы ввели недопустимые символы, дробное значение или не ввели ничего вовсе");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Указанный год не может быть меньше или равен нулю");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели недопустимые символы, дробное значение или не ввели ничего вовсе");
+            }
+            Console.ReadKey();
+        }
+    }
+}
